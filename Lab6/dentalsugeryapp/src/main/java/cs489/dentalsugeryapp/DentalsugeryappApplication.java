c740b2fbb55b9d15158566a1e1ecbdcff4c7a8e3
myommt <zai.myo.mintun@gmail.com>
2025-10-09 13:46:52 -0500

    Lab6

---
+++ Lab6/dentalsugeryapp/src/main/java/cs489/dentalsugeryapp/DentalsugeryappApplication.java
@@ -0,0 +1,13 @@
+package cs489.dentalsugeryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalsugeryappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentalsugeryappApplication.class, args);
+	}
+
+}
---
+++ Lab6/dentalsugeryapp/src/test/java/cs489/dentalsugeryapp/DentalsugeryappApplicationTests.java
@@ -0,0 +1,13 @@
+package cs489.dentalsugeryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalsugeryappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
